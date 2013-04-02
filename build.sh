9889b9542e3b8f890136fa83570c9875062a6c75
Oleg <romaxa@gmail.com>
2013-04-02 11:39:55 -0700

    Updated refs, added no tests qmake param

--- build.sh
+++ build.sh
@@ -202,8 +202,8 @@ build_qtmozembed()
 {
     check_sbox2
     # Build qtmozembed
-    echo "BUILD: cd $CDR/qtmozembed && $SB2_SHELL $TARGET_QMAKE -recursive OBJ_PATH=$CDR/$OBJTARGETDIR OBJ_BUILD_PATH=$OBJTARGETDIR CONFIG+=staticlib && cd $CDR"
-    cd $CDR/qtmozembed && $SB2_SHELL $TARGET_QMAKE -recursive OBJ_PATH=$CDR/$OBJTARGETDIR OBJ_BUILD_PATH=$OBJTARGETDIR CONFIG+=staticlib && cd $CDR
+    echo "BUILD: cd $CDR/qtmozembed && $SB2_SHELL $TARGET_QMAKE -recursive NO_TESTS=1 OBJ_PATH=$CDR/$OBJTARGETDIR OBJ_BUILD_PATH=$OBJTARGETDIR CONFIG+=staticlib && cd $CDR"
+    cd $CDR/qtmozembed && $SB2_SHELL $TARGET_QMAKE -recursive NO_TESTS=1 OBJ_PATH=$CDR/$OBJTARGETDIR OBJ_BUILD_PATH=$OBJTARGETDIR CONFIG+=staticlib && cd $CDR
     cd $CDR/qtmozembed && $SB2_SHELL make clean
     $SB2_SHELL make -j4 -C $CDR/qtmozembed
     RES=$?
