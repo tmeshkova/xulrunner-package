2c283c6deb803aad4d0a6d53c7cb2a230b5df97a
Tatiana Meshkova <tanya.meshkova@gmail.com>
2013-11-28 04:40:39 -0800

    Updated reference

--- pull.all.sh
+++ pull.all.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-#ENGINE_BRANCH=embedlite_aurora_26
+#ENGINE_BRANCH=embedlite_26
+#OTHER_BRANCH=embedlite_26
 ENGINE_BRANCH=embedlite
+OTHER_BRANCH=master
 git pull && git submodule init && git submodule update && git submodule status
 CURDIR=$PWD
-cd embedlite-components && git checkout master && git pull && cd $CURDIR
+cd embedlite-components && git checkout $OTHER_BRANCH && git pull && cd $CURDIR
 cd qmlmozbrowser && git checkout master && git pull && cd $CURDIR
-cd qtmozembed && git checkout master && git pull && cd $CURDIR
+cd qtmozembed && git checkout $OTHER_BRANCH && git pull && cd $CURDIR
 cd mozilla-central && git checkout $ENGINE_BRANCH && git fetch origin && git merge origin/$ENGINE_BRANCH && cd $CURDIR
 
