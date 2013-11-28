50b897ae7d0235d19205c5ceb81e47ed57ada244
Tatiana Meshkova <tanya.meshkova@gmail.com>
2013-11-28 04:53:33 -0800

    Updated script to merge and pull latest changes properly

--- pull.all.sh
+++ pull.all.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-#ENGINE_BRANCH=embedlite_26
-#OTHER_BRANCH=embedlite_26
 ENGINE_BRANCH=embedlite
 OTHER_BRANCH=master
+#ENGINE_BRANCH=embedlite_26
+#OTHER_BRANCH=embedlite_26
 git pull && git submodule init && git submodule update && git submodule status
 CURDIR=$PWD
-cd embedlite-components && git checkout $OTHER_BRANCH && git pull && cd $CURDIR
-cd qmlmozbrowser && git checkout master && git pull && cd $CURDIR
-cd qtmozembed && git checkout $OTHER_BRANCH && git pull && cd $CURDIR
-cd mozilla-central && git checkout $ENGINE_BRANCH && git fetch origin && git merge origin/$ENGINE_BRANCH && cd $CURDIR
+cd embedlite-components && echo embedlite-components && git checkout $OTHER_BRANCH && git fetch origin && git merge origin/$OTHER_BRANCH && cd $CURDIR
+cd qmlmozbrowser && echo qmlmozbrowser && git checkout master && git pull && cd $CURDIR
+cd qtmozembed && echo qtmozembed && git checkout $OTHER_BRANCH && git fetch origin && git merge origin/$OTHER_BRANCH && cd $CURDIR
+cd mozilla-central && echo mozilla-central && git checkout $ENGINE_BRANCH && git fetch origin && git merge origin/$ENGINE_BRANCH && cd $CURDIR
 
